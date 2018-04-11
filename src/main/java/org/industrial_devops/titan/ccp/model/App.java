977adb4c0e80e2f10eab7f07cf40b870061d7c6e
Sören Henning <post@soeren-henning.de>
2018-04-11 09:38:51 +0200

    initial maven project

---
+++ src/main/java/org/industrial_devops/titan/ccp/model/App.java
@@ -0,0 +1,13 @@
+package org.industrial_devops.titan.ccp.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
