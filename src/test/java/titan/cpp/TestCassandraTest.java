d4dfa4c80c669245193d3df18625421af4355d09
Sören Henning <post@soeren-henning.de>
2018-04-16 13:57:50 +0200

    added simple Cassandra unit test

---
+++ src/test/java/titan/cpp/TestCassandraTest.java
@@ -0,0 +1,122 @@
+package titan.cpp;
+
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.cassandra.service.EmbeddedCassandraService;
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.DataType;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.querybuilder.Insert;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.datastax.driver.core.querybuilder.Select;
+import com.datastax.driver.core.schemabuilder.Create;
+import com.datastax.driver.core.schemabuilder.DropKeyspace;
+import com.datastax.driver.core.schemabuilder.KeyspaceOptions;
+import com.datastax.driver.core.schemabuilder.SchemaBuilder;
+import com.google.common.collect.ImmutableMap;
+
+
+
+public class TestCassandraTest {
+	
+	private static final String KEYSPACE = "test";
+
+	private static Cluster cluster;
+	private Session session;
+	
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		EmbeddedCassandraServerHelper.startEmbeddedCassandra();
+		
+		TestCassandraTest.cluster = Cluster.builder()
+				.addContactPoint(EmbeddedCassandraServerHelper.getHost())
+				.withPort(EmbeddedCassandraServerHelper.getNativeTransportPort())
+				.build();
+	}
+	
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		cluster.close();
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		final Session session = EmbeddedCassandraServerHelper.getSession();
+		final KeyspaceOptions createKeyspace = SchemaBuilder.createKeyspace(KEYSPACE)
+				.ifNotExists()
+				.with().replication(ImmutableMap.of("class", "SimpleStrategy", "replication_factor", 1));
+		session.execute(createKeyspace);
+		
+		this.session = cluster.connect(KEYSPACE);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		//EmbeddedCassandraServerHelper.cleanEmbeddedCassandra();
+		
+		this.session.close();
+		this.session = null;
+		//this.cluster.close();
+		//this.cluster = null;
+		
+		final Session session = EmbeddedCassandraServerHelper.getSession();
+		final DropKeyspace dropKeyspace = SchemaBuilder.dropKeyspace(KEYSPACE).ifExists();
+		session.execute(dropKeyspace);
+	}
+
+	@Test
+	public void testCassandraRunning() {
+		final String tableName = "testtable";
+		final String keyName = "key";
+		final int keyValue = 3;
+		
+		final Create create = SchemaBuilder.createTable(tableName).ifNotExists().addPartitionKey(keyName, DataType.cint());
+		this.session.execute(create);
+
+		final Insert insert = QueryBuilder.insertInto(tableName).value(keyName, keyValue);
+		this.session.execute(insert);
+
+		final Select select = QueryBuilder.select(keyName).from(tableName);
+		final ResultSet resultSet = this.session.execute(select);
+		final List<Row> rows = resultSet.all();
+
+		assertEquals(1, rows.size());
+		assertEquals(keyValue, rows.get(0).getInt(keyName));
+	}
+	
+
+	@Test
+	public void testCassandraRunningSecondTime() {
+		final String tableName = "testtable";
+		final String keyName = "key";
+		final int keyValue = 3;
+
+		final Create create = SchemaBuilder.createTable(tableName).ifNotExists().addPartitionKey(keyName, DataType.cint());
+		this.session.execute(create);
+
+		final Insert insert = QueryBuilder.insertInto(tableName).value(keyName, keyValue);
+		this.session.execute(insert);
+
+		final Select select = QueryBuilder.select(keyName).from(tableName);
+		final ResultSet resultSet = this.session.execute(select);
+		final List<Row> rows = resultSet.all();
+
+		assertEquals(1, rows.size());
+		assertEquals(keyValue, rows.get(0).getInt(keyName));
+	}
+
+}
