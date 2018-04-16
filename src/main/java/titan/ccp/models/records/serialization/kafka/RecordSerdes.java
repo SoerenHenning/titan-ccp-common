8bde801ed0feb2e4972905882711f99e07775f36
Sören Henning <post@soeren-henning.de>
2018-04-16 13:57:26 +0200

    updated record serdes

--- src/main/java/titan/ccp/models/records/serialization/kafka/RecordSerdes.java
+++ src/main/java/titan/ccp/models/records/serialization/kafka/RecordSerdes.java
@@ -3,12 +3,17 @@ package titan.ccp.models.records.serialization.kafka;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 
+import titan.ccp.models.records.AggregatedPowerConsumptionRecord;
 import titan.ccp.models.records.PowerConsumptionRecord;
 
 public final class RecordSerdes {
 
 	public static final Serde<PowerConsumptionRecord> forPowerConsumptionRecord() {
 		return Serdes.serdeFrom(new PowerConsumptionRecordSerializer(), new PowerConsumptionRecordDeserializer());
+	}
+	
+	public static final Serde<AggregatedPowerConsumptionRecord> forAggregatedPowerConsumptionRecord() {
+		return Serdes.serdeFrom(new AggregatedPowerConsumptionRecordSerializer(), new AggregatedPowerConsumptionRecordDeserializer());
 	}
 	
 }
