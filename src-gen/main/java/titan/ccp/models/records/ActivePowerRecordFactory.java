17006167ce7db795b5b2b3faf171f4577bc85a00
Sören Henning <post@soeren-henning.de>
2018-11-02 15:43:34 +0100

    Manually update Kieker records

--- src-gen/main/java/titan/ccp/models/records/ActivePowerRecordFactory.java
+++ src-gen/main/java/titan/ccp/models/records/ActivePowerRecordFactory.java
@@ -1,29 +1,22 @@
 package titan.ccp.models.records;
 
-
 import kieker.common.exception.RecordInstantiationException;
 import kieker.common.record.factory.IRecordFactory;
 import kieker.common.record.io.IValueDeserializer;
 
 /**
  * @author Generic Kieker
- * 
+ *
  * @since 1.14
  */
 public final class ActivePowerRecordFactory implements IRecordFactory<ActivePowerRecord> {
-	
-	
+
 	@Override
 	public ActivePowerRecord create(final IValueDeserializer deserializer) throws RecordInstantiationException {
 		return new ActivePowerRecord(deserializer);
 	}
-	
+
 	@Override
-	@Deprecated
-	public ActivePowerRecord create(final Object[] values) {
-		return new ActivePowerRecord(values);
-	}
-	
 	public int getRecordSizeInBytes() {
 		return ActivePowerRecord.SIZE;
 	}
--- src-gen/main/java/titan/ccp/models/records/AggregatedActivePowerRecordFactory.java
+++ src-gen/main/java/titan/ccp/models/records/AggregatedActivePowerRecordFactory.java
@@ -1,29 +1,22 @@
 package titan.ccp.models.records;
 
-
 import kieker.common.exception.RecordInstantiationException;
 import kieker.common.record.factory.IRecordFactory;
 import kieker.common.record.io.IValueDeserializer;
 
 /**
  * @author Generic Kieker
- * 
+ *
  * @since 1.14
  */
 public final class AggregatedActivePowerRecordFactory implements IRecordFactory<AggregatedActivePowerRecord> {
-	
-	
+
 	@Override
 	public AggregatedActivePowerRecord create(final IValueDeserializer deserializer) throws RecordInstantiationException {
 		return new AggregatedActivePowerRecord(deserializer);
 	}
-	
+
 	@Override
-	@Deprecated
-	public AggregatedActivePowerRecord create(final Object[] values) {
-		return new AggregatedActivePowerRecord(values);
-	}
-	
 	public int getRecordSizeInBytes() {
 		return AggregatedActivePowerRecord.SIZE;
 	}
