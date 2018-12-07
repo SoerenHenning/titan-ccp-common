51a34f633a2ec7e1e8a3f27c73aca6684b59c2bc
Sören Henning <post@soeren-henning.de>
2018-12-07 10:20:42 +0100

    Add Cassandra adapter for Avro

---
+++ src/main/java/titan/ccp/common/avro/cassandra/AvroDataAdapter.java
@@ -0,0 +1,37 @@
+package titan.ccp.common.avro.cassandra;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.apache.avro.specific.SpecificRecord;
+import titan.ccp.common.cassandra.DataAdapter;
+
+public class AvroDataAdapter implements DataAdapter<SpecificRecord> {
+
+  @Override
+  public List<String> getFieldNames(final SpecificRecord record) {
+    return record.getSchema().getFields().stream().map(f -> f.name()).collect(Collectors.toList());
+  }
+
+  @Override
+  public List<Class<?>> getFieldTypes(final SpecificRecord record) {
+    final int numberOfFields = record.getSchema().getFields().size();
+    return IntStream.range(0, numberOfFields).mapToObj(i -> record.get(i).getClass())
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public List<Object> getValues(final SpecificRecord record) {
+    final int numberOfFields = record.getSchema().getFields().size();
+    final List<Object> values = new ArrayList<>(numberOfFields);
+    for (int i = 0; i < numberOfFields; i++) {
+      values.set(i, record.get(i));
+    }
+    return values;
+    // Probably less efficient:
+    // return IntStream.range(0, numberOfFields).mapToObj(i ->
+    // record.get(i).getClass()).collect(Collectors.toList());
+  }
+
+}
