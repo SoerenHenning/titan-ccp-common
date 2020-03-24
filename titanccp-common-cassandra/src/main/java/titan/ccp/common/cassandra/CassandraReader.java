fe9f61a685fe5d062b19531363eb97e489761586
Björn Vonheiden <bjoern.vonheiden@hotmail.de>
2020-03-24 15:37:11 +0100

    remove unused implementation

--- titanccp-common-cassandra/src/main/java/titan/ccp/common/cassandra/CassandraReader.java
+++ titanccp-common-cassandra/src/main/java/titan/ccp/common/cassandra/CassandraReader.java
@@ -11,36 +11,6 @@ import org.apache.avro.specific.SpecificRecordBase;
 import titan.ccp.common.avro.cassandra.AvroDataAdapter;
 
 public class CassandraReader {
-
-//  public static <T extends SpecificRecordBase> Function<Row, T> recordFactory(Supplier<T> factory) {
-//    
-//    // Prepare for getting Avro details
-//    AvroDataAdapter dataAdapter = new AvroDataAdapter();
-//    T template = factory.get();
-//    
-//    // Schema and Fields of Avro type
-//    Schema schema = template.getSchema();
-//    List<String> schemaFields = dataAdapter.getFieldNames(template);
-//    
-//    // Function for mapping from a row to a Avro object
-//    return (row) -> {
-//      // The new Avro object
-//      T avro = factory.get();
-//      
-//      // Decoder for the row
-//      RowDecoder rowDecoder = new RowDecoder(row, schemaFields);
-//      
-//      try {
-//        // Resolving decoder needed for the SpecificRedord
-//        ResolvingDecoder resolvingDecoder = DecoderFactory.get().resolvingDecoder(schema, schema, rowDecoder);
-//        avro.customDecode(resolvingDecoder);
-//      } catch (IOException e) {
-//        e.printStackTrace();
-//      }
-//      
-//      return avro;
-//    };
-//  }
   
   public static <T extends SpecificRecordBase> Function<Row, T> recordFactory(Supplier<T> factory) {
     
