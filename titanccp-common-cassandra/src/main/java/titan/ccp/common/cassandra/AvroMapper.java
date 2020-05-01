381be7321aef1e4dee172589dc888a8f54515245
Björn Vonheiden <bjoern.vonheiden@hotmail.de>
2020-05-01 11:34:30 +0200

    refactor code style

--- titanccp-common-cassandra/src/main/java/titan/ccp/common/cassandra/AvroMapper.java
+++ titanccp-common-cassandra/src/main/java/titan/ccp/common/cassandra/AvroMapper.java
@@ -11,52 +11,58 @@ import org.apache.avro.specific.SpecificRecordBase;
 import titan.ccp.common.avro.cassandra.AvroDataAdapter;
 import titan.ccp.common.avro.cassandra.RowDecoder;
 
+/**
+ * Class that enables mapping from a cassandra row to a Avro object.
+ *
+ * @param <T> The type of the Avro record to which should be mapped.
+ */
 public class AvroMapper<T extends SpecificRecordBase> implements Function<Row, T> {
-  
+
   private final Supplier<T> factory;
   private final List<String> schemaFields;
   private final SpecificDatumReader<T> specificDatumReader;
-  
+
 
   /**
-   * Creates a new {@code AvroMapper} that contains a mapping function from a Cassandra {@code Row} to an Avro Object.
+   * Creates a new {@code AvroMapper} that contains a mapping function from a Cassandra {@code Row}
+   * to an Avro Object.
+   * 
    * @param factory The function for creating a new avro Object.
    */
-  public AvroMapper(Supplier<T> factory) {
+  public AvroMapper(final Supplier<T> factory) {
     this.factory = factory;
-    
+
     // Prepare for getting Avro details
-    AvroDataAdapter dataAdapter = new AvroDataAdapter();
-    T template = factory.get();
-    
+    final AvroDataAdapter dataAdapter = new AvroDataAdapter();
+    final T template = factory.get();
+
     // Schema and Fields of Avro object
-    Schema schema = template.getSchema();
+    final Schema schema = template.getSchema();
     schemaFields = dataAdapter.getFieldNames(template);
-    
+
     // Creates later avro object
-    specificDatumReader = new SpecificDatumReader<T>(schema);
+    specificDatumReader = new SpecificDatumReader<>(schema);
   }
-  
+
   /**
    * A mapping function from a {@code Row} to an Avro object.
+   * 
    * @param row The cassandra {@code Row} where the function should be applied to.
    * @return A new Avro object.
    */
   @Override
-  public T apply(Row row) {
+  public T apply(final Row row) {
     // Decoder for the row
-    RowDecoder rowDecoder = new RowDecoder(row, schemaFields);
-    
-    //New Object
-    T avro = factory.get();
-    
+    final RowDecoder rowDecoder = new RowDecoder(row, schemaFields);
+
+    // New Object
+    final T avro = factory.get();
+
     try {
-      //Read in the data into the Avro object
-      avro = specificDatumReader.read(avro, rowDecoder);
+      // Read in the data into the Avro object
+      return specificDatumReader.read(avro, rowDecoder);
     } catch (IOException e) {
       throw new IllegalStateException("Could not map a cassandra row to an Avro object.", e);
     }
-    
-    return avro;
   }
 }
